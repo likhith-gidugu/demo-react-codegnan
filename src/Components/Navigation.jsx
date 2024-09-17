72111f9ccee0d8666f2e91b09ec1ad79941ac376
likhith-gidugu <likhithgidugu036.com>
2024-09-17 12:49:36 +0530

    first commit

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './stylings/Navigation.css'
+import './Stylings/Navigation.css'
 import { useNavigate } from 'react-router-dom'
 
 export default function Navigation() {
